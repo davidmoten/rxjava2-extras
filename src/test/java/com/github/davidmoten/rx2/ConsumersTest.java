b5546b67ac48ab52a71dc69ec42597a627792df4
Dave Moten <davidmoten@gmail.com>
2016-12-10 21:38:46 +1100

    add Consumers test

---
+++ src/test/java/com/github/davidmoten/rx2/ConsumersTest.java
@@ -0,0 +1,14 @@
+package com.github.davidmoten.rx2;
+
+import org.junit.Test;
+
+import com.github.davidmoten.junit.Asserts;
+
+public class ConsumersTest {
+    
+    @Test
+    public void testIsUtilityClass() {
+        Asserts.assertIsUtilityClass(Consumers.class);
+    }
+
+}
