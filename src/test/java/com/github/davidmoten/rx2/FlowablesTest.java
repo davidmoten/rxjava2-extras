61613b0cb3c2682b6a9b3f6cc229a03d5e0268ad
Dave Moten <davidmoten@gmail.com>
2017-01-28 21:17:10 +1100

    add FlowablesTest

---
+++ src/test/java/com/github/davidmoten/rx2/FlowablesTest.java
@@ -0,0 +1,14 @@
+package com.github.davidmoten.rx2;
+
+import org.junit.Test;
+
+import com.github.davidmoten.junit.Asserts;
+
+public class FlowablesTest {
+
+    @Test
+    public void isUtilityClass() {
+        Asserts.assertIsUtilityClass(Flowables.class);
+    }
+
+}
