b420304deedb87b38a2bc6fad31568d53957fd5e
Dave Moten <davidmoten@gmail.com>
2017-01-28 20:49:00 +1100

    add BiPredicates tests

--- src/test/java/com/github/davidmoten/rx2/BiPredicatesTest.java
+++ src/test/java/com/github/davidmoten/rx2/BiPredicatesTest.java
@@ -1,5 +1,6 @@
 package com.github.davidmoten.rx2;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import com.github.davidmoten.junit.Asserts;
@@ -9,6 +10,16 @@ public class BiPredicatesTest {
     @Test
     public void isUtilityClass() {
         Asserts.assertIsUtilityClass(BiPredicates.class);
+    }
+
+    @Test
+    public void testAlwaysTrue() throws Exception {
+        Assert.assertTrue(BiPredicates.alwaysTrue().test(new Object(), new Object()));
+    }
+
+    @Test
+    public void testAlwaysFalse() throws Exception {
+        Assert.assertFalse(BiPredicates.alwaysFalse().test(new Object(), new Object()));
     }
 
 }
