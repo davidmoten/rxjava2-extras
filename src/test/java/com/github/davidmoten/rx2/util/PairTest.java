bf88c029df752e6f20a1b0d240aa78d7e965ccf1
Dave Moten <davidmoten@gmail.com>
2017-01-28 21:26:49 +1100

    add coverage of Pair.equals

--- src/test/java/com/github/davidmoten/rx2/util/PairTest.java
+++ src/test/java/com/github/davidmoten/rx2/util/PairTest.java
@@ -1,5 +1,8 @@
 package com.github.davidmoten.rx2.util;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -29,5 +32,32 @@ public class PairTest {
     @Test
     public void testToString() {
         Assert.assertEquals("Pair [left=1, right=2]", Pair.create(1, 2).toString());
+    }
+
+    @Test
+    public void testEquals() {
+        Pair<Integer, Integer> a = Pair.create(1, 2);
+        Pair<Integer, Integer> b = Pair.create(1, 2);
+        Pair<Integer, Integer> c = Pair.create(1, 3);
+        Pair<Integer, Integer> d = Pair.create(3, 2);
+        Pair<Integer, Integer> e = Pair.create(3, 4);
+        Pair<Integer, Integer> f = Pair.create(1, null);
+        Pair<Integer, Integer> g = Pair.create(null, 2);
+        Pair<Integer, Integer> h = Pair.create(null, null);
+        Pair<Integer, Integer> i = Pair.create(null, null);
+        assertTrue(a.equals(b));
+        assertTrue(a.equals(a));
+        assertFalse(a.equals(c));
+        assertFalse(a.equals(d));
+        assertFalse(a.equals(e));
+        assertFalse(a.equals(null));
+        assertFalse(a.equals(f));
+        assertFalse(a.equals(g));
+        assertFalse(a.equals(1));
+        assertTrue(f.equals(f));
+        assertTrue(g.equals(g));
+        assertFalse(f.equals(a));
+        assertFalse(g.equals(a));
+        assertTrue(h.equals(i));
     }
 }
