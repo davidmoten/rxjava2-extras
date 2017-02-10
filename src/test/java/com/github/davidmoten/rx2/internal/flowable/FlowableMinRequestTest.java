13a1e3589c86be8237b57455358ef5bd09e87bf7
Dave Moten <davidmoten@gmail.com>
2017-02-10 11:40:24 +1100

    minRequest tests

--- src/test/java/com/github/davidmoten/rx2/internal/flowable/FlowableMinRequestTest.java
+++ src/test/java/com/github/davidmoten/rx2/internal/flowable/FlowableMinRequestTest.java
@@ -1,15 +1,20 @@
 package com.github.davidmoten.rx2.internal.flowable;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.junit.Test;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 
 import com.github.davidmoten.guavamini.Lists;
 import com.github.davidmoten.rx2.Consumers;
 import com.github.davidmoten.rx2.FlowableTransformers;
+import com.github.davidmoten.rx2.exceptions.ThrowingException;
 
 import io.reactivex.Flowable;
 
@@ -39,6 +44,69 @@ public class FlowableMinRequestTest {
                 .assertValues(1, 2, 3, 4, 5, 6, 7, 8, 9, 10) //
                 .assertComplete();
         assertEquals(Lists.newArrayList(1L, 2L, 2L, 2L, 2L, 2L), requests);
+    }
+
+    @Test
+    public void testMinWhenRequestIsMaxValue() {
+        List<Long> requests = new CopyOnWriteArrayList<Long>();
+        Flowable.range(1, 10) //
+                .doOnRequest(Consumers.addLongTo(requests)) //
+                .compose(FlowableTransformers.minRequest(2, true)) //
+                .test() //
+                .assertValues(1, 2, 3, 4, 5, 6, 7, 8, 9, 10) //
+                .assertComplete();
+        assertEquals(Lists.newArrayList(Long.MAX_VALUE), requests);
+    }
+
+    @Test
+    public void testError() {
+        List<Long> requests = new CopyOnWriteArrayList<Long>();
+        Flowable.<Long> error(new ThrowingException()) //
+                .doOnRequest(Consumers.addLongTo(requests)) //
+                .compose(FlowableTransformers.minRequest(2, true)) //
+                .test() //
+                .assertNoValues() //
+                .assertError(ThrowingException.class);
+        assertEquals(Lists.newArrayList(Long.MAX_VALUE), requests);
+    }
+
+    @Test
+    public void testCancel() {
+        List<Long> requests = new CopyOnWriteArrayList<Long>();
+        final List<Integer> values = new CopyOnWriteArrayList<Integer>();
+        final AtomicBoolean terminated = new AtomicBoolean();
+        Flowable.range(1, 10) //
+                .doOnRequest(Consumers.addLongTo(requests)) //
+                .compose(FlowableTransformers.<Integer> minRequest(2, true)) //
+                .subscribe(new Subscriber<Integer>() {
+
+                    private Subscription parent;
+
+                    @Override
+                    public void onSubscribe(Subscription parent) {
+                        this.parent = parent;
+                        parent.request(Long.MAX_VALUE);
+                    }
+
+                    @Override
+                    public void onNext(Integer t) {
+                        values.add(t);
+                        parent.cancel();
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+                        terminated.set(true);
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        terminated.set(true);
+                    }
+                });
+        assertEquals(Lists.newArrayList(1), values);
+        assertFalse(terminated.get());
+        assertEquals(Lists.newArrayList(Long.MAX_VALUE), requests);
     }
 
 }
