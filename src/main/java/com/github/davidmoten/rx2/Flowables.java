f257c250000bdb46eaa96cebaf36eefe22a08881
Dave Moten <davidmoten@gmail.com>
2017-02-11 14:31:50 +1100

    add paging

--- src/main/java/com/github/davidmoten/rx2/Flowables.java
+++ src/main/java/com/github/davidmoten/rx2/Flowables.java
@@ -1,11 +1,19 @@
 package com.github.davidmoten.rx2;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.atomic.AtomicLong;
+
 import com.github.davidmoten.rx2.internal.flowable.FlowableMatch;
 import com.github.davidmoten.rx2.internal.flowable.FlowableRepeat;
 
+import io.reactivex.BackpressureStrategy;
 import io.reactivex.Flowable;
 import io.reactivex.functions.BiFunction;
 import io.reactivex.functions.Function;
+import io.reactivex.functions.LongConsumer;
+import io.reactivex.internal.subscriptions.SubscriptionHelper;
+import io.reactivex.subjects.ReplaySubject;
+import rx.exceptions.Exceptions;
 
 public final class Flowables {
 
@@ -33,4 +41,46 @@ public final class Flowables {
         return new FlowableRepeat<T>(t, count);
     }
 
+    public static <T> Flowable<T> fetchPagesByRequest(final BiFunction<Long, Long, ? extends Flowable<T>> fetch,
+            long start, final int maxConcurrency) {
+        return Flowable.defer(new Callable<Flowable<T>>() {
+            @Override
+            public Flowable<T> call() throws Exception {
+                // need a ReplaySubject because requests can come through before
+                // concatEager has established subscriptions to the subject
+                final ReplaySubject<Flowable<T>> subject = ReplaySubject.create();
+                final AtomicLong position = new AtomicLong();
+                LongConsumer request = new LongConsumer() {
+                    @Override
+                    public void accept(long n) throws Exception {
+                        long start = position.getAndAdd(n);
+                        if (SubscriptionHelper.validate(n)) {
+                            Flowable<T> flowable;
+                            try {
+                                flowable = fetch.apply(start, n);
+                            } catch (Throwable e) {
+                                Exceptions.throwIfFatal(e);
+                                subject.onError(e);
+                                return;
+                            }
+                            subject.onNext(flowable);
+                        }
+                    }
+                };
+                return Flowable //
+                        .concatEager(subject.serialize() //
+                                .toFlowable(BackpressureStrategy.BUFFER), maxConcurrency, 128) //
+                        .doOnRequest(request);
+            }
+        });
+    }
+
+    public static <T> Flowable<T> fetchPagesByRequest(final BiFunction<Long, Long, ? extends Flowable<T>> fetch,
+            long start) {
+        return fetchPagesByRequest(fetch, start, 2);
+    }
+
+    public static <T> Flowable<T> fetchPagesByRequest(final BiFunction<Long, Long, ? extends Flowable<T>> fetch) {
+        return fetchPagesByRequest(fetch, 0, 2);
+    }
 }
--- src/test/java/com/github/davidmoten/rx2/FlowablesTest.java
+++ src/test/java/com/github/davidmoten/rx2/FlowablesTest.java
@@ -1,14 +1,56 @@
 package com.github.davidmoten.rx2;
 
+import java.util.function.LongFunction;
+
 import org.junit.Test;
 
 import com.github.davidmoten.junit.Asserts;
+import com.github.davidmoten.rx2.exceptions.ThrowingException;
+
+import io.reactivex.Flowable;
+import io.reactivex.functions.BiFunction;
 
 public class FlowablesTest {
 
     @Test
     public void isUtilityClass() {
         Asserts.assertIsUtilityClass(Flowables.class);
+    }
+
+    @Test
+    public void testFetchByRequest() {
+        final BiFunction<Long, Long, Flowable<Integer>> fetch = new BiFunction<Long, Long, Flowable<Integer>>() {
+            @Override
+            public Flowable<Integer> apply(Long start, Long n) {
+                return Flowable.just(n.intValue()).repeat(n);
+            }
+
+        };
+        Flowables.fetchPagesByRequest(fetch) //
+                .test(0) //
+                .assertNoValues() //
+                .requestMore(1) //
+                .assertValue(1) //
+                .requestMore(2) //
+                .assertValues(1, 2, 2) //
+                .requestMore(3) //
+                .assertValues(1, 2, 2, 3, 3, 3) //
+                .assertNotTerminated();
+    }
+
+    @Test
+    public void testFetchByRequestError() {
+        final BiFunction<Long, Long, Flowable<Integer>> fetch = new BiFunction<Long, Long, Flowable<Integer>>() {
+            @Override
+            public Flowable<Integer> apply(Long start, Long n) {
+                throw new ThrowingException();
+            }
+
+        };
+        Flowables.fetchPagesByRequest(fetch) //
+                .test(1) //
+                .assertNoValues() //
+                .assertError(ThrowingException.class);
     }
 
 }
