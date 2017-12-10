f14dc367da394d16d85f793f7d8f61699caaa15f
Dave Moten <davidmoten@gmail.com>
2017-12-10 16:14:17 +1100

    test dev

--- src/test/java/com/github/davidmoten/rx2/MpscQueue.java
+++ src/test/java/com/github/davidmoten/rx2/MpscQueue.java
@@ -4,30 +4,41 @@ import java.util.concurrent.atomic.AtomicReference;
 
 import com.github.davidmoten.guavamini.Preconditions;
 
-public class MpscQueue<T> {
+public final class MpscQueue<T> {
 
     private final AtomicReference<Node<T>> head = new AtomicReference<Node<T>>();
     private final AtomicReference<Boolean> tailSet = new AtomicReference<Boolean>(false);
+
+    // mutable
     private Node<T> tail;
 
-    private static class Node<T> {
+    private static final class Node<T> {
+
+        // mutable
+
         T value;
         Node<T> next;
 
-        public Node(T value) {
+        Node(T value) {
             this.value = value;
         }
     }
 
     public void push(T value) {
+        // performs one volatile read and two CAS operations per call to this method
+        // (under contention can be higher)
+
         Preconditions.checkNotNull(value, "cannot push a null value");
-        // CAS loop
         Node<T> node = new Node<T>(value);
         while (true) {
             Node<T> h = head.get();
             if (head.compareAndSet(h, node)) {
                 h.next = node;
                 if (tailSet.compareAndSet(false, true)) {
+                    // don't mind that this is not instantly visible to poll() because
+                    // in reactive operators the push is always followed by a drain that
+                    // forces a poll which will be ordered after this assignment (so this
+                    // assignment will be visible)
                     tail = h;
                 }
                 break;
@@ -36,18 +47,22 @@ public class MpscQueue<T> {
     }
 
     public T poll() {
+        // performs one volatile write only when reading the end of the queue
+
         if (tail == null) {
             return null;
         } else {
-            Node<T> tempTail = tail;
-            Node<T> next = tempTail.next;
+            Node<T> temp = tail;
+            Node<T> next = temp.next;
             if (next == null) {
                 tail = null;
                 tailSet.set(false);
+            } else {
+                tail = next;
             }
-            T v = tempTail.value;
+            T v = temp.value;
             // help gc including prevent nepotism
-            tempTail.value = null;
+            temp.value = null;
             return v;
         }
     }
