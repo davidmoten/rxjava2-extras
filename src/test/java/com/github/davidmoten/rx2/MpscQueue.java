e2c9aedb1d31100d6fdb190021aa5d6e33591366
Dave Moten <davidmoten@gmail.com>
2017-12-10 13:54:27 +1100

    test dev

--- src/test/java/com/github/davidmoten/rx2/MpscQueue.java
+++ src/test/java/com/github/davidmoten/rx2/MpscQueue.java
@@ -7,6 +7,7 @@ import com.github.davidmoten.guavamini.Preconditions;
 public class MpscQueue<T> {
 
     private final AtomicReference<Node<T>> head = new AtomicReference<Node<T>>();
+    private final AtomicReference<Boolean> tailSet = new AtomicReference<Boolean>(false);
     private Node<T> tail;
 
     private static class Node<T> {
@@ -21,12 +22,12 @@ public class MpscQueue<T> {
     public void push(T value) {
         Preconditions.checkNotNull(value, "cannot push a null value");
         // CAS loop
+        Node<T> node = new Node<T>(value);
         while (true) {
             Node<T> h = head.get();
-            Node<T> node = new Node<T>(value);
             if (head.compareAndSet(h, node)) {
                 h.next = node;
-                if (tail == null) {
+                if (tailSet.compareAndSet(false, true)) {
                     tail = h;
                 }
                 break;
@@ -38,10 +39,14 @@ public class MpscQueue<T> {
         if (tail == null) {
             return null;
         } else {
-            Node<T> next = tail.next;
-            T v = tail.value;
+            Node<T> tempTail = tail;
+            Node<T> next = tempTail.next;
+            if (next == null) {
+                tailSet.set(false);
+            }
+            T v = tempTail.value;
             // help gc including prevent nepotism
-            tail.value = null;
+            tempTail.value = null;
             tail = next;
             return v;
         }
