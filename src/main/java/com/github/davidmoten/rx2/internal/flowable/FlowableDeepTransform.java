2f728fedf5c3bf81dfb3eee9cf16079767d9a0ae
Dave Moten <davidmoten@gmail.com>
2018-05-08 17:24:13 +1000

    dev

--- src/main/java/com/github/davidmoten/rx2/internal/flowable/FlowableDeepTransform.java
+++ src/main/java/com/github/davidmoten/rx2/internal/flowable/FlowableDeepTransform.java
@@ -31,16 +31,16 @@ public class FlowableDeepTransform<T> extends Flowable<T> {
         source.subscribe(subscription);
     }
 
-    private static final class DeepTransformSubscriber<T> 
-            implements Subscription, Subscriber<T>, Runnable {
+    private static final class DeepTransformSubscriber<T> implements Subscription, Subscriber<T>, Runnable {
 
         private final BiFunction<Flowable<T>, Runnable, Flowable<T>> transform;
-        private final Subscriber<? super T> child;
-        private final AtomicInteger wip = new AtomicInteger();
-        private final AtomicLong requested = new AtomicLong();
-        private SimplePlainQueue<T> queue = createQueue();
-
+        final Subscriber<? super T> child;
         
+        //wip stays at 1 until doneCalled
+        private final AtomicInteger wip = new AtomicInteger(1);
+        private final AtomicLong requested = new AtomicLong();
+        SimplePlainQueue<T> queue = createQueue();
+        SimplePlainQueue<T> queue2 = createQueue();
         private Subscription parent;
         private final Flowable<T> source;
         private Throwable error;
@@ -49,7 +49,8 @@ public class FlowableDeepTransform<T> extends Flowable<T> {
         private volatile boolean doneCalled;
         private volatile boolean cancelled;
         private SimplePlainQueue<T> buffered;
-        
+        private long emitted;
+
         public DeepTransformSubscriber(Flowable<T> source, BiFunction<Flowable<T>, Runnable, Flowable<T>> transform,
                 Subscriber<? super T> child) {
             this.source = source;
@@ -92,24 +93,13 @@ public class FlowableDeepTransform<T> extends Flowable<T> {
         public void onError(Throwable t) {
             error = t;
             finished = true;
+            drain();
         }
 
         @Override
         public void onComplete() {
-            if (doneCalled) {
-                // emit all 
-                while (true) {
-                    T t = queue.poll();
-                    if (t != null) {
-                        child.onNext(t);
-                    } else {
-                        child.onComplete();
-                        break;
-                    }
-                }
-            } else {
-                
-            }
+            finished = true;
+            drain();
         }
 
         @Override
@@ -121,6 +111,10 @@ public class FlowableDeepTransform<T> extends Flowable<T> {
             if (wip.getAndIncrement() == 0) {
                 int missed = 1;
                 while (true) {
+                    long r = requested.get();
+                    long e = emitted;
+                    boolean d = doneCalled;
+                    
                     break;
                 }
                 missed = wip.addAndGet(-missed);
@@ -135,10 +129,121 @@ public class FlowableDeepTransform<T> extends Flowable<T> {
             // TODO Auto-generated method stub
             // complete so report what's buffered and pass through future emissions
             doneCalled = true;
+            wip.set(0);
+            drain();
+        }
+
+        public void transformCompleted() {
+            if (doneCalled) {
+                // emit all
+                while (true) {
+                    T t = queue.poll();
+                    if (t != null) {
+                        child.onNext(t);
+                    } else {
+                        child.onComplete();
+                        break;
+                    }
+                }
+            } else {
+                
+            }
+        }
+
+        public void transformErrored(Throwable t) {
+            // TODO Auto-generated method stub
+            
         }
 
     }
-    
+
+    private static final class FlowableBetweenQueues<T> extends Flowable<T> implements Subscriber<T>, Subscription {
+
+        private final DeepTransformSubscriber<T> dt;
+        private final AtomicLong requested = new AtomicLong();
+        private long emitted;
+        private final AtomicInteger wip = new AtomicInteger();
+        private volatile boolean cancelled;
+
+        private Subscription transformed;
+        private Subscriber<? super T> subscriber;
+
+        FlowableBetweenQueues(DeepTransformSubscriber<T> dt) {
+            super();
+            this.dt = dt;
+        }
+
+        @Override
+        protected void subscribeActual(Subscriber<? super T> subscriber) {
+            this.subscriber = subscriber;
+            subscriber.onSubscribe(this);
+        }
+
+        @Override
+        public void onSubscribe(Subscription s) {
+            transformed = s;
+            transformed.request(Long.MAX_VALUE);
+        }
+
+        @Override
+        public void onNext(T t) {
+            dt.queue2.offer(t);
+        }
+
+        @Override
+        public void onError(Throwable t) {
+            dt.transformErrored(t);
+        }
+
+        @Override
+        public void onComplete() {
+            dt.transformCompleted();
+        }
+
+        @Override
+        public void request(long n) {
+            if (SubscriptionHelper.validate(n)) {
+                BackpressureHelper.add(requested, n);
+                drain();
+            }
+        }
+
+        private void drain() {
+            if (wip.getAndIncrement() == 0) {
+                int missed = 1;
+                while (true) {
+                    long r = requested.get();
+                    long e = emitted;
+                    while (e != r) {
+                        if (cancelled) {
+                            dt.queue.clear();
+                            return;
+                        }
+                        T t = dt.queue.poll();
+                        if (t == null) {
+                            subscriber.onComplete();
+                            return;
+                        } else {
+                            subscriber.onNext(t);
+                            e++;
+                        }
+                    }
+                    emitted = e;
+                    missed = wip.addAndGet(-missed);
+                    if (missed == 0) {
+                        return;
+                    }
+                }
+            }
+        }
+
+        @Override
+        public void cancel() {
+            cancelled = true;
+        }
+
+    }
+
     private static <T> SimplePlainQueue<T> createQueue() {
         return new SpscLinkedArrayQueue<T>(16);
     }
